a3f76c5f6e5494a4aeb9f1a48ac9b3c065d230bb
Chung Haw Tseng <chunghaw@Chungs-MacBook-Pro.local>
2021-04-01 23:33:03 +0900

    [FEAT] Created configuration file for the server and db

--- src/pkg/config/configuration.go
+++ src/pkg/config/configuration.go
@@ -7,3 +7,39 @@ import(
 
 var Config *Configuration
 
+type Configuration struct {
+	Server   ServerConfiguration
+	// Database DatabaseConfiguration
+}
+
+type ServerConfiguration struct {
+	Port   string
+	Secret string
+	Mode   string
+}
+
+
+// SetupDB initialize configuration
+func Setup(configPath string) {
+	var configuration *Configuration
+
+	// Decoding the config file
+	viper.SetConfigFile(configPath)
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+
+	Config = configuration
+}
+
+// GetConfig helps you to get configuration data
+func GetConfig() *Configuration {
+	return Config
+}
