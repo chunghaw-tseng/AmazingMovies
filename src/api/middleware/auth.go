2d606de43df99230d443fd4cde0582708dda3278
Chung Haw Tseng <chunghaw@Chungs-MacBook-Pro.local>
2021-04-01 23:24:40 +0900

    [FEAT] Added authentication middleware and error handling middleware

--- src/api/middleware/auth.go
+++ src/api/middleware/auth.go
@@ -1,2 +1,32 @@
 package middleware
 
+import (
+	"example.com/amazingmovies/pkg/crypto"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+
+// Checks and depends on the API will check any of the bottom
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authorizationHeader := c.GetHeader("authorization")
+		// TODO Check if authorization is a API Key or a token
+		if !crypto.ValidateToken(authorizationHeader) {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			return
+		} else {
+			c.Next()
+		}
+	}
+}
+
+
+func TokenRequired(){
+
+}
+
+// TODO Check the API
+func APIKeyRequired(){
+
+}
