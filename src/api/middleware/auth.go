698180f493d5e68ff43da41267a6d8aaff175113
Chung Haw Tseng <chunghaw@Chungs-MacBook-Pro.local>
2021-04-06 23:01:18 +0900

    [FEAT] Added Admin checks middleware

--- src/api/middleware/auth.go
+++ src/api/middleware/auth.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"example.com/amazingmovies/src/pkg/persistence"
 	"example.com/amazingmovies/pkg/crypto"
 	"github.com/gin-gonic/gin"
@@ -27,14 +26,12 @@ func AuthRequired() gin.HandlerFunc {
 					if user , err := s.GetByUsername(username); err != nil {
 						c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized : token not accepted"})
 					} else {
-						fmt.Println("User obtained ", user)
 						c.Set("user", user)
 						c.Next()
 					}
 				}
 			}else{
 					// API Key
-					fmt.Println("API Key ", authorizationHeader)
 					if user, err := s.GetbyKey(authorizationHeader); err != nil {
 						c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized : api_key not valid"})
 					} else {
@@ -42,6 +39,41 @@ func AuthRequired() gin.HandlerFunc {
 						c.Next()
 					}
 				}
+		}else{
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "No authorization header found"})
+		}
+	}
+}
+
+// Checks if user is admin for deleting
+func AdminRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Check if the token is a Bearer token
+		authorizationHeader := strings.TrimSpace(c.GetHeader("authorization"))
+		s := persistence.GetUserRepository()
+		if authorizationHeader != "" {
+			// Token need to return user info
+			if strings.Contains(authorizationHeader, "Bearer"){
+				auth_slice := strings.Fields(authorizationHeader)
+				if valid, username := crypto.ValidateToken(auth_slice[1]); !valid{
+					c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized : token not accepted"})
+					return
+				} else {
+					if user , err := s.GetByUsername(username); err != nil {
+						c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized : token not accepted"})
+					} else {
+						if user.RoleID == 1{
+							c.Set("user", user)
+							c.Next()
+							return
+						}else{
+							c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized : Admin Role Required"})
+						}
+					}
+				}
+			}else{
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not Bearer Token Found"})
+			}
 		}else{
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "No authorization header found"})
 		}
